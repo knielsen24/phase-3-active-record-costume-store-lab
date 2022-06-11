4134c05123fe6af5aad669bc3ca44d4d8abd3eca
Kevin Nielsen <knielsen24@gmail.com>
2022-06-11 10:47:29 -0700

    commit tree dc90e6c8f4f1cd3b7329397d4d8a98e616998359

---
+++ db/migrate/20220611173343_create_costumes.rb
@@ -0,0 +1,18 @@
+class CreateCostumes < ActiveRecord::Migration[6.1]
+	def change
+		create_table :costumes do |t|
+			t.string :name
+			t.float :price
+			t.string :size
+			t.string :image_url
+
+			t.timestamps
+		end
+
+  	end
+end
+
+# name (string)
+# price (float)
+# size (string)
+# image_url (string)
